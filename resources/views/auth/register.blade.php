f79d82e6a89f5661eb1e5db06fd03ecd7bfc6317
RafiAwes <rafiaweshan4897@gmail.com>
2023-10-03 14:31:50 +0600

    registration commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,61 @@
-<x-guest-layout>
+@extends('auth.master')
+@section('content')
+<div class="pt-5">
+    <section class="signup">
+        <div class="container">
+            <div class="signup-content">
+                <div class="signup-form">
+                    <h2 class="form-title">Sign up</h2>
+                    <form method="POST" class="register-form" id="register-form" action="{{ route('register') }}">
+                        @csrf
+                        <div class="form-group">
+                            <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                            <x-text-input id="name" class="#" type="text" name="name" placeholder="Your Name"  required autocomplete="name" />
+                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                            {{-- <input type="text" name="name" id="name" placeholder="Your Name"/> --}}
+                        </div>
+                        <div class="form-group">
+                            <label for="email"><i class="zmdi zmdi-email"></i></label>
+                            <x-text-input id="email" class="#" type="email" name="email" placeholder="Your Email" required  autocomplete="username" />
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+
+                            {{-- <input type="email" name="email" id="email" placeholder="Your Email"/> --}}
+                        </div>
+                        <div class="form-group">
+                            <label for="password"><i class="zmdi zmdi-lock"></i></label>
+                            <x-text-input id="password" class="#" type="password" placeholder="Password" name="password" required autocomplete="new-password" />
+
+                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                            {{-- <input type="password" name="pass" id="pass" placeholder="Password"/> --}}
+                        </div>
+                        <div class="form-group">
+                            <label for="password_confirmation"><i class="zmdi zmdi-lock-outline"></i></label>
+                            <x-text-input id="password_confirmation" class="#" type="password" name="password_confirmation" placeholder="Repeat your password" required autocomplete="new-password" />
+                            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                            {{-- <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password"/> --}}
+                        </div>
+                        {{-- <div class="form-group">
+                            <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
+                            <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
+                        </div> --}}
+                        <div class="form-group form-button">
+                            <input type="submit" name="signup" id="signup" class="form-submit" value="{{ __('Register') }}"/>
+                        </div>
+                    </form>
+                </div>
+                <div class="signup-image">
+                    <figure><img src="{{url('/')}}/log_regassets/images/signup-image.jpg" alt="sing up image"></figure>
+                    <a href="{{ route('login') }}" class="signup-image-link">I am already member</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</div>
+
+@endsection
+
+{{-- <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -49,4 +106,4 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
